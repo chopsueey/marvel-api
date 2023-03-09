6330ca2df3b704d89b9eb25b30573348450198cd
chopsueey <meyer-marius@live.de>
2023-03-09 10:02:22 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -24,31 +24,7 @@ function App() {
           </section>
         </div>
         <Characters />
-        <div className="row d-flex justify-content-center">
-        <div
-          className="text-center d-flex flex-column col-md-6"
-          style={{ color: "white" }}
-        >
-          <h2>Hey, großer Bruder, ich gratuliere dir herzlichst zu deinem Geburtstag!</h2>
-          <p>Ich wünsche dir Erfolg, Gesundheit und Ausdauer auf deinem Weg
-            zum Abschluss der Ausbildung und in privaten Dingen. Ich hoffe für dich,
-            dass alles so klappen wird, wie du es dir vorstellst und wünschst.
-          
-          </p>
-          <p>
-            Und ja... wie du siehst, habe ich eine kleine Website gebaut.
-            Du kannst oben Charaktere von
-            Marvel suchen und dir zum Beispiel deren Comics anschauen. Ich
-            baue über die Wochen noch mehr ein und ändere noch das Design, weil
-            ich die Website dann als Projekt vorstelle. Aber die Grundfunktion
-            klappt erstmal. Vielleicht hast du ja noch Ideen, was man einbauen
-            kann. :D
-            (Manche links klappen nicht, aber das liegt anscheinend an veralteten Server-Einträgen.)
-            
-          </p>
-          <p>Liebe Grüße, Marius</p>
-        </div>
-        </div>
+        
         {/* <Button onClick={loadMoreHandler}>load more</Button> */}
       </div>
     </div>
